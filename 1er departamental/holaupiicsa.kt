379da5968f6498fbb53cf09c403522dd0bac046a
FranciscoGarciaJasso2000 <146609220+FranciscoGarciaJasso2000@users.noreply.github.com>
2023-10-03 12:48:53 -0600

    Update holaupiicsa.kt

--- 1er departamental/holaupiicsa.kt
+++ 1er departamental/holaupiicsa.kt
@@ -1,6 +1,6 @@
 fun main() {
     // Mensaje Hola upiicsa
-    print("Hello, world!!!")
+    print("Hola mundo!")
     
     //Se imprime cualquier otro mensaje 
     println("Ay si la ven me la saludan")
