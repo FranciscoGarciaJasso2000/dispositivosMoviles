5d4d2a0e49aca608f4b7e9a5486e6584e2561a1d
FranciscoGarciaJasso2000 <fgarciaj2000@alumno.ipn.mx>
2023-10-03 13:01:04 -0600

    Primer departamental

---
+++ 1er departamental/HelloUPIICSA.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Impresión del Hello UPIICSA
+    print("Hello UPIICSA")  
+    
+    // Println para hacer un salto de línea
+    println("HolaMundo")
+    
+    // 3er mensaje 
+    println("Mi primer programa escrito en Kotlin")
+}
